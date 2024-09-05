f2a947540330a5c6e19c4241a1f000cf4adca1d9
jpashley1 <jpashley1@gmail.com>
2024-09-05 12:49:25 -0400

    added functional methods to my products_controller.rb file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,69 @@
 class ProductsController < ApplicationController
   def index
-    render json: { message: "index" }
+    @products = Product.all
+    # render "products/index"
+    render json: @products
+    # ask why this doesn't work: render :index
+    # render :index
+
+    # # testing route code:
+    # render json: { message: "index" }
   end
 
   def show
-    render json: { message: "Show" }
+    # get the right id
+    @product = Product.find_by(id: params[:id])
+    # find the product with that id
+    # ask why this doesn't work: render :show
+    # ask why the singular versus plural, is ruby just smart, or am I missing a key peice of logic regarding the convention
+    render json: @product
+    # # testing route code:
+    # render json: { message: "Show" }
   end
 
   def create
-    render json: { message: "create" }
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+      stock: params[:stock],
+
+    )
+    @product.save
+    render json: @product
+
+    # # testing route code:
+    # render json: { message: "create" }
   end
 
   def update
-    render json: { message: "update" }
+    # find the correct product
+    @product = Product.find_by(id: params[:id])
+    # modify attributes
+    #
+    # what should i do if the param is nil? I want to not update that value
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
+      stock: params[:stock],
+    )
+    render json: @product
+    # # testing route code:
+    # render json: { message: "update" }
   end
 
   def destroy
-    render json: { message: "destroy" }
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "product has been removed" }
+    # # testing route code:
+    # render json: { message: "destroy" }
   end
 
   # def handle_root_not_found
   #   render json: { message: "route not found" }, status: 404
-  # end
-
-  # def index
-  #   @products = Product.all
-  #   render template: "products/index"
   # end
 end
